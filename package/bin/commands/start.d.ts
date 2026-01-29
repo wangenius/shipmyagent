e1b4d994fa1eb3e882f02cf2bcb9da84b6ce4ad0
wangzhenggao <wzg003110@163.com>
2026-01-29 14:13:51 +0800

    update: git

--- package/bin/commands/start.d.ts
+++
@@ -1,7 +0,0 @@
-interface StartOptions {
-    port: number;
-    host: string;
-}
-export declare function startCommand(cwd: string | undefined, options: StartOptions): Promise<void>;
-export {};
-//# sourceMappingURL=start.d.ts.map
